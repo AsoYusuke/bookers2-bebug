a4d2d6565319bc8b9ff4596958ad230328f6fe49
AsoYusuke <aso.aso.1227@i.softbank.jp>
2021-04-08 12:09:14 +0000

    コメント機能非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,30 @@
 class BookCommentsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = @book.id
-    comment.save
-    redirect_to book_path(@book)
+    @book_comment = BookComment.new
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @book }
+        format.js { render :index }
+      else
+        format.html { redirect_to @book }
+      end
+    end
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book_comment = BookComment.new
+    respond_to do |format|
+      if BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+        format.html { redirect_to @book }
+        format.js { render :index }
+      else
+        format.html { redirect_to @book }
+      end
+    end
   end
 
   private
