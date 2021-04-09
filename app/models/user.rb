b85722fb89feb63a76d3905c9cd962e6a49fe40d
AsoYusuke <aso.aso.1227@i.softbank.jp>
2021-04-09 19:15:11 +0000

    住所検索機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -48,5 +51,13 @@ class User < ApplicationRecord
       @user = User.all
     end
   end 
+  
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+  
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
   
 end
